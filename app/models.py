1279367e06136a58a6f13e62abc83044223968b8
Igor <igoroshust75@gmail.com>
2024-09-03 21:23:49 +0900

    intermediate code version before review

--- app/models.py
+++ app/models.py
@@ -160,3 +160,28 @@ class OneTimeCode(models.Model):
 #             settings.DEFAULT_FROM_EMAIL,
 #             [recipient],
 #         )
+
+@receiver(post_save, sender=Response)
+def send_response_notification(sender, instance, created, **kwargs):
+    if created:
+        publication = instance.publication
+        emails = instance.author.email
+
+        subject = 'Новый отклик на Вашу публикацию'
+
+        text_content = (
+            f'Статья: {publication.name} <br>'
+            f'Дата публикации: {publication.date} <br><br>'
+            f'Ссылка на статью: http://127.0.0.1:8000'
+        )
+
+        html_content = (
+            f'Статья: {publication.name} <br>'
+            f'<a href="http://127.0.0.1:8000/{publication.get_absolute_url()}"'
+            f'Перейти по ссылке</a>'
+        )
+
+        for email in emails:
+            msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [email])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
